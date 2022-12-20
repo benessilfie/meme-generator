01c0fa7f17b08c31a14cf3272b796732c310678a
benessilfie <bquansah007@gmail.com>
2022-12-20 00:12:06 +0000

    Add images to meme generator with useEffect

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import memesData from "../memesData";
 
 function Meme() {
-  function getMeme() {
+  let [memeImage, setMemeImage] = useState("https://i.imgflip.com/1bij.jpg");
+
+  function getMemeImage() {
     const memesArr = memesData.data.memes;
     const randomMeme = memesArr[Math.floor(Math.random() * memesArr.length)];
-    console.log(randomMeme.url);
+
+    setMemeImage(randomMeme.url);
   }
 
   return (
@@ -12,10 +16,11 @@ function Meme() {
       <div className="form">
         <input type="text" placeholder="Top-text" className="form__input" />
         <input type="text" placeholder="Bottom-text" className="form__input" />
-        <button className="form__button" onClick={getMeme}>
+        <button className="form__button" onClick={getMemeImage}>
           Get me a new meme image
         </button>
       </div>
+      <img src={memeImage} alt="meme image" className="meme__image" />
     </main>
   );
 }
